1c99cb65752f3dbffed9b0ac93fe58f875b8a168
Peng Hao <flyingpeng@tencent.com>
2022-03-27 15:39:27 +0800

    rustfmt: code style adjustment
    Signed-off-by: Peng Hao <flyingpeng@tencent.com>

--- samples/rust/rust_kvm/rust_kvm.rs
+++ samples/rust/rust_kvm/rust_kvm.rs
@@ -3,11 +3,10 @@
 //! Rust KVM for VMX
 //#![feature(asm)]
 #[allow(dead_code)]
-
 use kernel::prelude::*;
 use kernel::{
     file::File,
-    file_operations::{FileOperations, IoctlCommand, IoctlHandler}, 
+    file_operations::{FileOperations, IoctlCommand, IoctlHandler},
     miscdev,
     sync::{CondVar, Mutex, Ref, RefBorrow, UniqueRef},
 };
@@ -38,11 +37,11 @@ struct VmxState {
 
 // used for vmxon
 struct VmxInfo {
-  revision_id: u32,
-  region_size: u16,
-  write_back: bool,
-  io_exit_info: bool,
-  vmx_controls: bool,
+    revision_id: u32,
+    region_size: u16,
+    write_back: bool,
+    io_exit_info: bool,
+    vmx_controls: bool,
 }
 /*
 impl VmxInfo {
@@ -51,12 +50,30 @@ impl VmxInfo {
 */
 impl VmxState {
     fn try_new() -> Result<Ref<Self>> {
-	pr_info!("VmxState try_new \n");
+        pr_info!("VmxState try_new \n");
         let val: u64 = 0;
         let mut state = Pin::from(UniqueRef::try_new(Self {
             // SAFETY: `condvar_init!` is called below.
-            host_state: HostState{rsp: 0, xcr0: 0},
-            guest_state: GuestState{ rax: val, rcx: val, rdx: val, rbx: val, rbp: val, rsi: val, rdi: val, r8: val, r9: val, r10: val, r11: val, r12: val, r13: val, r14: val, r15: val, cr2: val, xcr0: val },
+            host_state: HostState { rsp: 0, xcr0: 0 },
+            guest_state: GuestState {
+                rax: val,
+                rcx: val,
+                rdx: val,
+                rbx: val,
+                rbp: val,
+                rsi: val,
+                rdi: val,
+                r8: val,
+                r9: val,
+                r10: val,
+                r11: val,
+                r12: val,
+                r13: val,
+                r14: val,
+                r15: val,
+                cr2: val,
+                xcr0: val,
+            },
             state_changed: unsafe { CondVar::new() },
             // SAFETY: `mutex_init!` is called below.
             inner: unsafe { Mutex::new(SharedStateInner { token_count: 0 }) },
@@ -82,13 +99,13 @@ impl FileOperations for KVM {
     kernel::declare_file_operations!(ioctl);
 
     fn open(shared: &Ref<VmxState>, _file: &File) -> Result<Self::Wrapper> {
-        pr_info!("KVM open \n");	
+        pr_info!("KVM open \n");
         Ok(shared.clone())
     }
 
-    fn ioctl(shared: RefBorrow<'_, VmxState>,file: &File, cmd: &mut IoctlCommand) -> Result<i32> {
+    fn ioctl(shared: RefBorrow<'_, VmxState>, file: &File, cmd: &mut IoctlCommand) -> Result<i32> {
         cmd.dispatch::<VmxState>(&shared, file)
-    }        
+    }
 }
 
 struct RustMiscdev {
@@ -101,7 +118,7 @@ impl KernelModule for RustMiscdev {
 
         let state = VmxState::try_new()?;
         /* vmxon percpu*/
-        
+
         Ok(RustMiscdev {
             _dev: miscdev::Registration::new_pinned(name, state)?,
         })
@@ -114,11 +131,8 @@ impl Drop for RustMiscdev {
     }
 }
 
-
-
 const IOCTL_KVM_CREATE_VM: u32 = 0x00AE0100;
 const IOCTL_KVM_CREATE_VCPU: u32 = 0x00AE4100;
-
 
 impl IoctlHandler for VmxState {
     type Target<'a> = &'a VmxState;
@@ -130,9 +144,9 @@ impl IoctlHandler for VmxState {
                 Ok(0)
             }
             IOCTL_KVM_CREATE_VCPU => {
-		pr_info!("Rust kvm: IOCTL_KVM_CREATE_VCPU\n");
-		Ok(0)
-	    }
+                pr_info!("Rust kvm: IOCTL_KVM_CREATE_VCPU\n");
+                Ok(0)
+            }
             _ => Err(Error::EINVAL),
         }
     }
